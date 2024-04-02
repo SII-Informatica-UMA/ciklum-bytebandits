d5f6bb28be15d4758147308b7c9b131d41887872
Carmen Alonso <96998039+carmenaalonso@users.noreply.github.com>
2024-04-02 13:36:40 +0200

    Update detalle-contacto.component.spec.ts

--- src/app/detalle-contacto/detalle-contacto.component.spec.ts
+++ src/app/detalle-contacto/detalle-contacto.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DetalleContactoComponent } from './detalle-contacto.component';
+import { Dieta } from '../dieta';
 
-
-describe('El componente de detalle de contacto', () => {
+describe('El componente de detalle de dieta', () => {
   let component: DetalleContactoComponent;
   let fixture: ComponentFixture<DetalleContactoComponent>;
 
@@ -16,36 +16,91 @@ describe('El componente de detalle de contacto', () => {
     component = fixture.componentInstance;
   });
 
-  it('debe mostrar el atributo "género" debajo del teléfono', () => {
-    component.contacto = {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: '', genero: 'Hombre'};
+  it('debe mostrar la descripción de la dieta', () => {
+    const dieta: Dieta = {
+      id: 1,
+      nombre: 'Dieta 1',
+      descripcion: 'Descripción de la dieta 1',
+      observaciones: 'Observaciones de la dieta 1',
+      objetivo: 'Objetivo de la dieta 1',
+      duracionDias: 30,
+      alimentos: 'Alimentos de la dieta 1',
+      recomendaciones: 'Recomendaciones de la dieta 1'
+    };
+    component.dieta = dieta;
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('#telefono + div')).not.toBeNull();
-    expect(compiled.querySelector('#telefono + div label')?.textContent).toContain('Género');
+    expect(compiled.querySelector('#Descripcion')?.textContent).toContain(dieta.descripcion);
   });
 
-  it('debe mostrar el género para los contactos (probando con Mujer)', () => {
-    component.contacto = {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: '', genero: 'Mujer'};
+  it('debe mostrar las observaciones de la dieta', () => {
+    const dieta: Dieta = {
+      id: 1,
+      nombre: 'Dieta 1',
+      descripcion: 'Descripción de la dieta 1',
+      observaciones: 'Observaciones de la dieta 1',
+      objetivo: 'Objetivo de la dieta 1',
+      duracionDias: 30,
+      alimentos: 'Alimentos de la dieta 1',
+      recomendaciones: 'Recomendaciones de la dieta 1'
+    };
+    component.dieta = dieta;
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('#telefono + div')).not.toBeNull();
-    expect(compiled.querySelector('#telefono + div span')?.textContent).toContain('Mujer');
+    expect(compiled.querySelector('#Observaciones')?.textContent).toContain(dieta.observaciones);
   });
 
-  it('debe mostrar el género para los contactos (probando con Hombre)', () => {
-    component.contacto = {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: '', genero: 'Hombre'};
+  it('debe mostrar la duración de la dieta', () => {
+    const dieta: Dieta = {
+      id: 1,
+      nombre: 'Dieta 1',
+      descripcion: 'Descripción de la dieta 1',
+      observaciones: 'Observaciones de la dieta 1',
+      objetivo: 'Objetivo de la dieta 1',
+      duracionDias: 30,
+      alimentos: 'Alimentos de la dieta 1',
+      recomendaciones: 'Recomendaciones de la dieta 1'
+    };
+    component.dieta = dieta;
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('#telefono + div')).not.toBeNull();
-    expect(compiled.querySelector('#telefono + div span')?.textContent).toContain('Hombre');
+    expect(compiled.querySelector('#Duracion')?.textContent).toContain(dieta.duracionDias);
   });
 
-  it('debe mostrar el género para los contactos (probando con la cadena vacía)', () => {
-    component.contacto = {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: '', genero: ''};
+  it('debe mostrar los alimentos de la dieta', () => {
+    const dieta: Dieta = {
+      id: 1,
+      nombre: 'Dieta 1',
+      descripcion: 'Descripción de la dieta 1',
+      observaciones: 'Observaciones de la dieta 1',
+      objetivo: 'Objetivo de la dieta 1',
+      duracionDias: 30,
+      alimentos: 'Alimentos de la dieta 1',
+      recomendaciones: 'Recomendaciones de la dieta 1'
+    };
+    component.dieta = dieta;
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('#telefono + div')).not.toBeNull();
-    expect(compiled.querySelector('#telefono + div span')?.textContent).toEqual('');
+    expect(compiled.querySelector('#Alimentos')?.textContent).toContain(dieta.alimentos);
+  });
+
+  it('debe mostrar las recomendaciones de la dieta', () => {
+    const dieta: Dieta = {
+      id: 1,
+      nombre: 'Dieta 1',
+      descripcion: 'Descripción de la dieta 1',
+      observaciones: 'Observaciones de la dieta 1',
+      objetivo: 'Objetivo de la dieta 1',
+      duracionDias: 30,
+      alimentos: 'Alimentos de la dieta 1',
+      recomendaciones: 'Recomendaciones de la dieta 1'
+    };
+    component.dieta = dieta;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#Recomendaciones')?.textContent).toContain(dieta.recomendaciones);
   });
 
 });
+
+
