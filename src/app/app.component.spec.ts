18ee433a6d5ea69c3a0d5d6256ac31fc3b08fd33
Charly_8 <117116268+CarlosCorbOrd@users.noreply.github.com>
2024-04-02 13:54:10 +0200

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,17 @@
-import { FormularioContactoComponent } from './formulario-contacto/formulario-contacto.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {ContactosService } from './dieta.service';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {Contacto} from './dieta';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { DietaService } from './dieta.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Dieta } from './dieta';
 
 class MockNgbModal {
   modalRef = {
     componentInstance: {
-      contacto: {id: 0, nombre: '', apellidos: '', email: '', telefono: ''},
-      accion: 'Añadir'},
-    result: Promise.resolve({id: 0, nombre: '', apellidos: '', email: '', telefono: ''})};
+      dieta: { id: 0, nombre: '', descripcion: '', observaciones: '', objetivo: '', duracionDias: 0, alimentos: '', recomendaciones: '' },
+      accion: 'Añadir'
+    },
+    result: Promise.resolve({ id: 0, nombre: '', descripcion: '', observaciones: '', objetivo: '', duracionDias: 0, alimentos: '', recomendaciones: '' })
+  };
 
   open() {
     return this.modalRef;
@@ -21,36 +19,34 @@ class MockNgbModal {
 }
 
 describe('El componente principal', () => {
-  let mockService: ContactosService;
+  let mockService: DietaService;
   let mockModal: MockNgbModal;
   let fixture: ComponentFixture<AppComponent>;
   let compiled: HTMLElement;
-  let eddd=0
 
   beforeEach(async () => {
     mockService = {
-      getContactos: () => {
+      getDietas: () => {
         return [
-          {id: 1, nombre: 'Dieta1', apellidos: 'Una dieta equilibrada incluye una variedad de alimentos nutritivos como proteínas magras, carbohidratos complejos, grasas saludables y vitaminas/minerales, evitando el exceso de azúcares y alimentos procesados. Es crucial ajustar las porciones según las necesidades individuales y mantener un equilibrio entre la ingesta de calorías y el gasto energético.', email: '', telefono: ''},
-          {id: 2, nombre: 'Dieta2', apellidos: 'Una dieta equilibrada incluye una variedad de alimentos nutritivos como proteínas magras, carbohidratos complejos, grasas saludables y vitaminas/minerales, evitando el exceso de azúcares y alimentos procesados. Es crucial ajustar las porciones según las necesidades individuales y mantener un equilibrio entre la ingesta de calorías y el gasto energético.', email: '', telefono: ''}]
+          { id: 1, nombre: 'Dieta1', descripcion: 'Descripción de la dieta 1', observaciones: 'Observaciones de la dieta 1', objetivo: 'Objetivo de la dieta 1', duracionDias: '30', alimentos: 'Alimentos de la dieta 1', recomendaciones: 'Recomendaciones de la dieta 1' },
+          { id: 2, nombre: 'Dieta2', descripcion: 'Descripción de la dieta 2', observaciones: 'Observaciones de la dieta 2', objetivo: 'Objetivo de la dieta 2', duracionDias: '30', alimentos: 'Alimentos de la dieta 2', recomendaciones: 'Recomendaciones de la dieta 2' }
+        ];
       },
-      eliminarContacto: (id: number) => {},
-      editarContacto: (contacto: Contacto) => {eddd+=1},
-      addContacto: (contacto: Contacto) => {}
-    } as ContactosService;
+      eliminarDieta: (id: number) => {},
+      editarDieta: (dieta: Dieta) => {},
+      addDieta: (dieta: Dieta) => {}
+    } as DietaService;
 
     mockModal = new MockNgbModal();
 
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
-        FormularioContactoComponent
+        AppComponent
       ],
-      imports: [BrowserModule,
-        FormsModule],
       providers: [
-        {provide: ContactosService, useValue: mockService},
-        {provide: NgbModal, useValue: mockModal}]
+        { provide: DietaService, useValue: mockService },
+        { provide: NgbModal, useValue: mockModal }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
@@ -58,85 +54,19 @@ describe('El componente principal', () => {
     compiled = fixture.nativeElement as HTMLElement;
   });
 
-  it('debe tener una tabla', () => {
-    expect(compiled.querySelectorAll('table')).toHaveSize(1);
+  it('debe mostrar todas las dietas en la lista de botones', () => {
+    const buttons = compiled.querySelectorAll('.list-group button');
+    expect(buttons.length).toBe(2);
+    expect(buttons[0].textContent).toContain('Dieta1');
+    expect(buttons[1].textContent).toContain('Dieta2');
   });
 
-  it('debe mostrar a Ana en primer lugar en la tabla', () => {
-    expect(compiled.querySelector('tbody tr:first-child td:first-child')?.textContent).toBe('Ana');
+
+  it('el botón de añadir debe abrir el formulario vacío', () => {
+    const button = compiled.querySelector('.btn-outline-primary.bi-plus-lg.mb-4') as HTMLElement;
+    const spyOpen = spyOn(mockModal, 'open').and.callThrough();
+    button.click();
+    expect(spyOpen).toHaveBeenCalled();
   });
-
-  it('debe mostrar a Juan en segundo lugar en la lista de botones', () => {
-    expect(compiled.querySelector('tbody tr:nth-child(2) td:first-child')?.textContent).toBe('Juan');
-  });
-
-  it('debe haber dos botones por fila', () => {
-    expect(compiled.querySelectorAll('tbody tr').length).not.toBe(0);
-    compiled.querySelectorAll('tbody tr').forEach((fila) => {
-      expect(fila.querySelectorAll('td button')).toHaveSize(2);
-    });
-  });
-
-  it('el segundo botón debe eliminar un contacto', (done: DoneFn) => {
-    let spy = spyOn(mockService, 'eliminarContacto').and.callThrough();
-    const botonEliminar = compiled.querySelector('tbody tr:nth-child(1) td:last-child button:last-child') as HTMLElement
-    botonEliminar.click();
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      expect(spy).toHaveBeenCalled();
-      done();
-    });
-  });
-
-  it('el primer botón debe abrir el formulario para editar el contacto', (done: DoneFn) => {
-    const botonEditar = compiled.querySelector('tbody tr:nth-child(1) td:last-child button:nth-last-child(2)') as HTMLElement
-    mockModal.modalRef.result = Promise.resolve({id: 1, nombre: 'Juan', apellidos: 'Pérez', email: 'perez@uma.es', telefono: '1234'});
-    const spyEditar = spyOn(mockService, 'editarContacto').and.callThrough();
-
-    botonEditar.click();
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      expect(spyEditar).toHaveBeenCalled();
-      done();
-    });
-  });
-
-  it('debe haber un botón para añadir un contacto', () => {
-    expect(compiled.querySelector('table ~ button')).not.toBeNull();
-  });
-
-  it('el botón para añadir debe abrir el formulario vacío', (done: DoneFn) => {
-    const botonAniadir = compiled.querySelector('table ~ button') as HTMLElement
-    mockModal.modalRef.result = Promise.resolve({id: 3, nombre: 'Antonio', apellidos: 'García', email: '', telefono: ''});
-    const spyAniadir = spyOn(mockService, 'addContacto').and.callThrough();
-
-    botonAniadir.click();
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      expect(spyAniadir).toHaveBeenCalled();
-      done();
-    });
-
-  });
-
-  it('al insertar un nuevo contacto se debe añadir en su lugar de acuerdo con el orden por nombre', (done: DoneFn) => {
-    let carla: Contacto = {id: 3, nombre: 'Carla', apellidos: 'Fernández', email: '', telefono: ''};
-    const spyEditar = spyOn(mockService, 'getContactos').and.returnValue([
-      {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: ''},
-      {id: 2, nombre: 'Ana', apellidos: 'García', email: '', telefono: ''},
-      carla
-    ])
-
-    const botonAniadir = compiled.querySelector('table ~ button') as HTMLElement
-    mockModal.modalRef.result = Promise.resolve(carla);
-
-    botonAniadir.click();
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      fixture.detectChanges();
-      expect(compiled.querySelector('tbody tr:nth-child(2) td:first-child')?.textContent).toBe('Carla');
-      done();
-    });
-  });
-
 });
+
