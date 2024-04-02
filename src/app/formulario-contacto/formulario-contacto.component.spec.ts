6edb1f7c2615b0de4a25c312000a390f294dd14c
Carmen Alonso <96998039+carmenaalonso@users.noreply.github.com>
2024-04-02 13:38:05 +0200

    Update formulario-contacto.component.spec.ts

--- src/app/formulario-contacto/formulario-contacto.component.spec.ts
+++ src/app/formulario-contacto/formulario-contacto.component.spec.ts
@@ -3,106 +3,158 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormularioContactoComponent } from './formulario-contacto.component';
 import { FormsModule } from '@angular/forms';
 
-describe('El formulario de contactos', () => {
+describe('El formulario de dietas', () => {
   let component: FormularioContactoComponent;
   let fixture: ComponentFixture<FormularioContactoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports:[FormsModule],
-      declarations: [ FormularioContactoComponent ],
+      imports: [FormsModule],
+      declarations: [FormularioContactoComponent],
       providers: [NgbActiveModal]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(FormularioContactoComponent);
     component = fixture.componentInstance;
-
   });
 
-  it('debe mostrar una lista de selección debajo del teléfono', () => {
+  it('debe mostrar el nombre de la dieta', () => {
     component.accion = "Añadir";
     fixture.detectChanges();
 
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('#telefono + label')).not.toBeNull();
-    expect(compiled.querySelector('#telefono + label')?.textContent).toContain('Género');
-    expect(compiled.querySelector('#telefono ~ select')).not.toBeNull();
+    expect(compiled.querySelector('label[for="nombre"]')).not.toBeNull();
+    expect(compiled.querySelector('label[for="nombre"]')?.textContent).toContain('Nombre:');
+    expect(compiled.querySelector('input#nombre')).not.toBeNull();
   });
 
-  it('debe mostrar el seleciconado el género del contacto (probando Hombre)', (done: DoneFn) => {
+
+  it('debe cambiar el modelo cuando se modifica el nombre de la dieta', (done: DoneFn) => {
     component.accion = "Añadir";
-    component.contacto = {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: '', genero: 'Hombre'};
     fixture.detectChanges();
 
     const compiled = fixture.nativeElement as HTMLElement;
-    const lista = compiled.querySelector('#telefono ~ select') as HTMLInputElement;
+    const inputNombre = compiled.querySelector('input#nombre') as HTMLInputElement;
+    const nuevoNombre = 'Nueva Dieta';
+
+    inputNombre.value = nuevoNombre;
+    inputNombre.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
     fixture.whenStable().then(() => {
-      expect(lista.getAttribute('ng-reflect-model')).toBe('Hombre');
+      expect(component.dieta.nombre).toBe(nuevoNombre);
       done();
     });
   });
 
-  it('debe mostrar el seleciconado el género del contacto (probando Mujer)', (done: DoneFn) => {
+  it('debe cambiar el modelo cuando se modifica la descripción de la dieta', (done: DoneFn) => {
     component.accion = "Añadir";
-    component.contacto = {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: '', genero: 'Mujer'};
     fixture.detectChanges();
-
+  
     const compiled = fixture.nativeElement as HTMLElement;
-    const lista = compiled.querySelector('#telefono ~ select') as HTMLInputElement;
+    const inputDescripcion = compiled.querySelector('input#descripcion') as HTMLInputElement;
+    const nuevaDescripcion = 'Nueva descripción para la dieta';
+  
+    inputDescripcion.value = nuevaDescripcion;
+    inputDescripcion.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+  
     fixture.whenStable().then(() => {
-      expect(lista.getAttribute('ng-reflect-model')).toBe('Mujer');
+      expect(component.dieta.descripcion).toBe(nuevaDescripcion);
       done();
     });
   });
-
-  it('debe mostrar el seleciconado el género del contacto (pobando cadena vacía)', (done: DoneFn) => {
+  
+  it('debe cambiar el modelo cuando se modifica el objetivo de la dieta', (done: DoneFn) => {
     component.accion = "Añadir";
-    component.contacto = {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: '', genero: ''};
     fixture.detectChanges();
-
+  
     const compiled = fixture.nativeElement as HTMLElement;
-    const lista = compiled.querySelector('#telefono ~ select') as HTMLInputElement;
+    const inputObjetivo = compiled.querySelector('input#objetivo') as HTMLInputElement;
+    const nuevoObjetivo = 'Perder peso';
+  
+    inputObjetivo.value = nuevoObjetivo;
+    inputObjetivo.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+  
     fixture.whenStable().then(() => {
-      expect(lista.getAttribute('ng-reflect-model')).toBe('');
+      expect(component.dieta.objetivo).toBe(nuevoObjetivo);
       done();
     });
   });
-
-
-  it('debe cambiar el modelo cuando se cambia el género', (done: DoneFn) => {
+  
+  it('debe cambiar el modelo cuando se modifica la observaciones de la dieta', (done: DoneFn) => {
     component.accion = "Añadir";
-    component.contacto = {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: '', genero: ''};
     fixture.detectChanges();
-
+  
     const compiled = fixture.nativeElement as HTMLElement;
-    const lista = compiled.querySelector('#telefono ~ select') as HTMLInputElement;
-    const opcionAMarcar = compiled.querySelector('option:nth-child(3)') as HTMLInputElement;
-    lista.value = opcionAMarcar.value;
-    lista.dispatchEvent(new Event('change'));
+    const inputObservaciones = compiled.querySelector('input#observaciones') as HTMLInputElement;
+    const nuevasObservaciones = 'Nuevas observaciones para la dieta';
+  
+    inputObservaciones.value = nuevasObservaciones;
+    inputObservaciones.dispatchEvent(new Event('input'));
     fixture.detectChanges();
-
+  
     fixture.whenStable().then(() => {
-      expect(component.contacto.genero).toBe(opcionAMarcar.value);
+      expect(component.dieta.observaciones).toBe(nuevasObservaciones);
       done();
     });
   });
-
-  it('debe cambiar el modelo cuando se cambia el género', (done: DoneFn) => {
+  
+  it('debe cambiar el modelo cuando se modifica la duración en días de la dieta', (done: DoneFn) => {
     component.accion = "Añadir";
-    component.contacto = {id: 1, nombre: 'Juan', apellidos: 'Pérez', email: '', telefono: '', genero: ''};
     fixture.detectChanges();
-
+  
     const compiled = fixture.nativeElement as HTMLElement;
-    const lista = compiled.querySelector('#telefono ~ select') as HTMLInputElement;
-    const opcionAMarcar = compiled.querySelector('option:nth-child(11)') as HTMLInputElement;
-    lista.value = opcionAMarcar.value;
-    lista.dispatchEvent(new Event('change'));
+    const inputDuracionDias = compiled.querySelector('input#duracionDias') as HTMLInputElement;
+    const nuevaDuracionDias = 10;
+  
+    inputDuracionDias.value = nuevaDuracionDias.toString();
+    inputDuracionDias.dispatchEvent(new Event('input'));
     fixture.detectChanges();
-
+  
     fixture.whenStable().then(() => {
-      expect(component.contacto.genero).toBe(opcionAMarcar.value);
+      expect(component.dieta.duracionDias).toBe(nuevaDuracionDias);
       done();
     });
   });
+  
+  it('debe cambiar el modelo cuando se modifica la lista de alimentos de la dieta', (done: DoneFn) => {
+    component.accion = "Añadir";
+    fixture.detectChanges();
+  
+    const compiled = fixture.nativeElement as HTMLElement;
+    const inputAlimentos = compiled.querySelector('input#alimentos') as HTMLInputElement;
+    const nuevosAlimentos = 'Nuevos alimentos para la dieta';
+  
+    inputAlimentos.value = nuevosAlimentos;
+    inputAlimentos.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+  
+    fixture.whenStable().then(() => {
+      expect(component.dieta.alimentos).toBe(nuevosAlimentos);
+      done();
+    });
+  });
+  
+  it('debe cambiar el modelo cuando se modifica las recomendaciones de la dieta', (done: DoneFn) => {
+    component.accion = "Añadir";
+    fixture.detectChanges();
+  
+    const compiled = fixture.nativeElement as HTMLElement;
+    const inputRecomendaciones = compiled.querySelector('input#recomendaciones') as HTMLInputElement;
+    const nuevasRecomendaciones = 'Nuevas recomendaciones para la dieta';
+  
+    inputRecomendaciones.value = nuevasRecomendaciones;
+    inputRecomendaciones.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+  
+    fixture.whenStable().then(() => {
+      expect(component.dieta.recomendaciones).toBe(nuevasRecomendaciones);
+      done();
+    });
+  });
+  
 });
+
