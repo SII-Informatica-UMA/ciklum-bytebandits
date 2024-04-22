0e38dd5d15b870818817be137e0c4a05796045a6
juandf03 <115293960+juandf03@users.noreply.github.com>
2024-04-22 21:19:07 +0200

    Creación desde cero T2

---
+++ entidades/src/test/java/ciklumBytebandits/entidades/EntidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package ciklumBytebandits.entidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
