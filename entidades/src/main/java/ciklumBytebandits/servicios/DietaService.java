37f8a5ec9e0a3b26ae8d3d9a34133d19bccd0f9b
Carmen Alonso <96998039+carmenaalonso@users.noreply.github.com>
2024-05-07 10:01:45 +0200

    Update DietaService.java

--- entidades/src/main/java/ciklumBytebandits/servicios/DietaService.java
+++ entidades/src/main/java/ciklumBytebandits/servicios/DietaService.java
@@ -1,5 +1,49 @@
 package ciklumBytebandits.servicios;
 
+import ciklumBytebandits.entidades.Dieta;
+import ciklumBytebandits.excepciones.DietaException;
+import ciklumBytebandits.repositories.DietaRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class DietaService {
-    
+    private DietaRepository dietaRepo;
+
+    public DietaService(DietaRepository dietaRepository) {
+        this.dietaRepo = dietaRepo;
+    }
+
+    public List<Dieta> dietasDeEntrenador(Long entrenadorId) {
+        return this.dietaRepo.findAllByIdEntrenador(entrenadorId);
+    }
+
+    public List<Dieta> dietasDeCliente(Long clienteId) {
+        return this.dietaRepo.findByIdClientesContaining(clienteId);
+    }
+
+    public Dieta crearActualizarDieta(Dieta g) {
+        return (Dieta)this.dietaRepo.save(g);
+    }
+
+    public Optional<Dieta> obtenerDieta(Long id) {
+        return this.dietaRepo.findById(id);
+    }
+
+    public void asociarDieta(Long dietaID, Long clienteId) {
+        Optional<Dieta> d = this.obtenerDieta(dietaID);
+        d.ifPresent((dieta) -> {
+            dieta.getIdClientes().add(clienteId);
+            this.dietaRepo.save(dieta);
+        });
+        d.orElseThrow(DietaException::new);
+    }
+
+    public void eliminarDieta(Long id) {
+        this.dietaRepo.deleteById(id);
+    }
 }
+
