c893a3b692db73b4536f4ae171ac7714e71c247b
juandf03 <115293960+juandf03@users.noreply.github.com>
2024-05-07 12:52:18 +0200

    Estructura tests

--- entidades/src/test/java/ciklumBytebandits/entidades/EntidadesApplicationTests.java
+++ entidades/src/test/java/ciklumBytebandits/entidades/EntidadesApplicationTests.java
@@ -2,12 +2,110 @@ package ciklumBytebandits.entidades;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
+
+import ciklumBytebandits.repositories.DietaRepository;
+import ciklumBytebandits.dtos.*;
+import ciklumBytebandits.entidades.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DisplayName("En el servicio de dietas")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class EntidadesApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Value(value="${local.server.port}")
+	private int port;
+
+	@Autowired
+	private DietaRepository dietaRepo;
+
+	private URI uri(String scheme, String host, int port, String ...paths) {
+		UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+		UriBuilder ub = ubf.builder()
+				.scheme(scheme)
+				.host(host).port(port);
+		for (String path: paths) {
+			ub = ub.path(path);
+		}
+		return ub.build();
+	}
+
+	private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.get(uri)
+				.accept(MediaType.APPLICATION_JSON)
+				.build();
+		return peticion;
+	}
+
+	private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.delete(uri)
+				.build();
+		return peticion;
+	}
+
+	private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.post(uri)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(object);
+		return peticion;
+	}
+
+	private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.put(uri)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(object);
+		return peticion;
+	}
+
+	private void compruebaCampos(Ingrediente expected, Ingrediente actual) {
+		assertThat(actual.getNombre()).isEqualTo(expected.getNombre());
+	}
+
+	private void compruebaCampos(Producto expected, Producto actual) {	
+		assertThat(actual.getNombre()).isEqualTo(expected.getNombre());
+		assertThat(actual.getDescripcion()).isEqualTo(expected.getDescripcion());
+		assertThat(actual.getIngredientes()).isEqualTo(expected.getIngredientes());
+	}
+
+	@Nested
+	@DisplayName("cuando la base de datos está vacía")
+	public class BaseDatosVacia {
+
+	}
+
+	@Nested
+	@DisplayName("cuando la base de datos tiene datos")
+	public class BaseDatosConDatos {
+		
 	}
 
 }
